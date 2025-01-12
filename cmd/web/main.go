9a8ae3f68340187a5cb59078510a25ba0cabe093
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-01-12 16:59:07 +0100

    start snippetbox project

---
+++ cmd/web/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/devphaseX/snippetbox.git/internal/models"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+
+	snippets models.SnippetStore
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "", "MySQL data source name")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	if len(*dsn) == 0 {
+		errorLog.Panic("missing dsn flag")
+	}
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		snippets: &models.SnippetModel{
+			DB: db,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
